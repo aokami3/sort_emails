3b24605876f96860d15bba3d88e1bffc9b35d145
aokami3 <173888077+aokami3@users.noreply.github.com>
2025-05-09 12:22:39 +0900

    動作を修正

--- src/mailmove.py
+++ src/mailmove.py
@@ -1,237 +1,289 @@
+# 必要なモジュールをインポート
+import pythoncom
+import signal
+import sys
+import time
 import win32com.client
 import json
 import re
 import os
-import datetime
-# import schedule
+from datetime import datetime
+from collections import defaultdict
+from watchfiles import watch  # ファイル監視用のライブラリ
+import threading # スレッド処理用のライブラリ
+from flask import Flask
 
-# Outlook関係のオブジェクト初期化
-app = win32com.client.Dispatch("Outlook.Application")
-root = app.Session.DefaultStore.GetRootFolder() #アプリに設定されているパス
-ns = app.GetNamespace("MAPI")
-inbox = ns.GetDefaultFolder(6)
-messages = inbox.Items
+# ローカルフォルダへ保存するためのベースディレクトリ(** 適宜書き換えてください **)
+BASE_DIR = R"C:\Users\pcrik\Documents\メールバックアップ"
+data_ready = threading.Event()
 
-# ローカルフォルダへ保存するためのパスを定義
-dir_path = R"C:\Users\pcrik\Documents\メールバックアップ" # Rはパスのエスケープシーケンスに必要
-local_path = {}
+app = Flask(__name__)
 
-# メールをナンバリングするためのカウンター変数
-mail_counter = {}
+@app.route('/')
+def index():
+    """標準出力を表示するページ"""
+    return "メール監視中..."
 
-# Outlookのフォルダ検索
-def findfolder(root, name):
-    for folder in root.Folders:
-        # フォルダ名の部分一致
-        if name in folder.name:
-            # print(folder.folderpath)
-            return folder
-        # フォルダにない場合、サブフォルダも検索(再起関数)
-        ret = findfolder(folder, name)
-        if ret is not None:
-            return ret
-    return None
+# メールを保存するためのクラス
+class MailSaver:
+    def __init__(self, base_dir):
+        self.base_dir = base_dir
+        self.counters = defaultdict(int)  # フォルダごとのカウンターを管理
 
-# 条件適合判定
-# mail.jsonのアドレスには差出人名または差出人アドレス、宛先メールアドレスが入っている(差出人メールアドレスは必要か？後で検討)
-def isit(message, subjects, addresses=[]): # addressesには何が入ってるん？mail.jsonで定義したフォルダのメールアドレス（リスト）が入る
-    # 件名（正規表現）での判定
-    for subject in subjects:
-        # reライブラリのsearchメソッドを使用
-        if re.search(subject, message.subject)!= None:
-            return True
-    # メールアドレス（部分一致）判定
-    for address in addresses:
-        # 差出人名
-        if address in message.sendername: # 送り主の中に事前に定義した名前があれば真
-            return True
-        # 差出人アドレス
-        if address in message.senderemailaddress:
-            return True
-        # 宛先
-        # for recip in message.recipients:
-        #     if address in recip.name or address in recip.address:
-        #         return True
-    return False
+    def save_message(self, message, folder_path):
+        """メールをローカルフォルダに保存"""
+        self.counters[folder_path] += 1
+        folder_name = message.ReceivedTime.strftime("%Y_%m%d") + f"_{self.counters[folder_path]:05d}"
+        full_path = os.path.join(folder_path, folder_name)
+        os.makedirs(full_path, exist_ok=True)
 
-# アーカイブ先フォルダ検索
-def whichFolder(message, dic):
-    # 会議案内などは除外(IPM.Noteのみに処理を行う。IPM.Noteは電子メールメッセージ意味する)
-    if message.messageClass == "IPM.Note":
-        for key in dic:
-            if isit(message, dic[key]["subject"], dic[key]["address"]): # isit()関数の中でもループしているため、フォルダとフォルダのアドレスと件名が多いほど実行時間がかかる
-                return key
-    return None
+        # 添付ファイルの保存
+        if message.Attachments.Count > 0:
+            self.save_attachments(os.path.join(full_path, "attachments"), message)
 
-# JSONファイルから移動条件をロードする(最初の一回のみ実行すれば良い, 追加した場合のアップデートが必要になる可能性あり)
-def load_json(filename="mail.json"):
-    with open("mail.json", "r", encoding = "utf-8") as f:
-        dic = json.load(f)
-        folders = {}
-        for k in dic:
-            # 識別名の先頭が$ならコメントとして扱う
-            if not k.startswith('$'):
-                folders[k] = findfolder(root, dic[k]["folder"]) # Noneの場合はフォルダが見つからなかったことを表す
-    return dic, folders
+        # メール本文の保存
+        self.save_email_content(os.path.join(full_path, "本文.txt"), message)
 
-# メールのアーカイブ処理メイン部
-def move_mail(
-    dic, folders, target_folder=inbox, view_none=True, view_move=True, view_delete=True
-):
-    i = 1
-    counter_move = 0
-    counter_remain = 0
-    counter_delete = 0
-    list_move = list()
-    for message in target_folder.Items:#target_folderは受信トレイを指定
-        # print(len(target_folder.Items)) # これで受信フォルダにあるメールの件数が分かる
-        key = whichFolder(message, dic) # dicはload_json()で定義済み、keyはメールの件名等から判断した仕分け先フォルダ名(mail.jsonで定義した一意な識別子)を格納
-        # print(key) 
-        if key == "del": # keyはフォルダ名。messageに適したフォルダがあったらフォルダ名が返ってきて、無かったらNoneが帰ってくる
-            counter_delete += 1
-            list_move.append((message, None))
-        elif ( # 事前に設定していないメールの種類はそのまま残しておき、出力する
-            key is None
-            or folders[key] is None
-            or folders[key].folderpath == target_folder.folderpath
-        ):
-            counter_remain += 1
-            list_move.append((message, folders["archive"])) # 事前定義されていないメールはアーカイブへ移動
-            if view_none: # 通常時はTrue, do_all_folder関数の時のみFalse
-                print(counter_remain, "none", message.subject) # 仕分け未登録の件名を出力
-        elif dic[key]["unread"] or not message.unread: # 移動の際、未読を対象にするか？未読を対象にしない場合、message.unread == Flase
-            counter_move += 1
-            if view_move: #移動するものを出力するか? ログ用に使用
-                print(folders[key].name, message.subject)
-            list_move.append((message, folders[key]))
-        else: # 未読を対象にしない場合かつ、メールが未読の場合は残します
-            counter_remain += 1
-            if view_none: # Noneとなったメールの件名を出力します
-                print("unread", message.subject)
-        i += 1
-    for item in list_move:
-        message = item[0]
-        dest = item[1]
+    @staticmethod
+    def save_attachments(folder_path, message):
+        """添付ファイルを保存"""
+        os.makedirs(folder_path, exist_ok=True)
+        for attachment in message.Attachments:
+            attachment_file_path = os.path.join(folder_path, attachment.FileName)
+            attachment.SaveAsFile(attachment_file_path)
+            print(f"添付ファイルを保存しました: {attachment_file_path}")
+
+    @staticmethod
+    def save_email_content(file_path, message):
+        """メール内容をファイルに書き込む"""
+        header_fields = [
+            ("送信日時", message.SentOn),
+            ("送信者", f"{message.SenderName} <{message.SenderEmailAddress}>"),
+            # ("宛先 (To)", message.To or "宛先なし"),
+            ("CC", message.CC or "CCなし"),
+            ("BCC", message.BCC or "BCCなし"),
+            ("件名", message.Subject or "件名なし")
+        ]
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write("=== メール基本情報 ===\n")
+            for label, value in header_fields:
+                if value:
+                    f.write(f"・{label}: {value}\n")
+            f.write("\n=== 本文 ===\n")
+            f.write(f"{message.Body or '本文なし'}\n")
+        print(f"メールを保存しました: {file_path}")
+
+# メールを仕分けして保存するためのクラス
+class MailProcessor:
+    def __init__(self, base_dir=BASE_DIR, json_file=R"json/mail.json", root=None):
+        self.root = root
+        self.base_dir = base_dir
+        self.json_file = json_file
+        self.folders = {}
+        self.rules = {}
+        self.local_paths = {}
+        self.saver = MailSaver(base_dir)
+
+    def load_rules(self):
+        """JSONファイルから仕分けルールをロード"""
         try:
-            mail_counter[str(dest)] += 1
-        except KeyError:
-            mail_counter[str(dest)] = 1
-        if dest is None:
-            if view_delete:
-                print("delete", message.subject)
-            message.delete()
-        else:
-            print(dest.name, message.subject)
-            message.unread = False
-            message.move(dest)
+            with open(self.json_file, "r", encoding="utf-8") as f:
+                self.rules = json.load(f)
+                print("仕分けルールを読み込みました。")
+                return 0
+        except Exception as e:
+            print(f"JSONファイルの読み込み中にエラーが発生しました: {e}")
+            self.rules = {}
+            return 1
 
-            # 保存先フォルダを作成
-            folder_name = message.ReceivedTime.strftime("%Y_%m%d") + f"_{mail_counter[str(dest)]:05d}"
-            folder_path = os.path.join(local_path[str(dest)], folder_name)
-            os.makedirs(folder_path, exist_ok=True)
+    def find_folder(self, parent_folder, folder_name):
+        """指定されたフォルダ名を持つフォルダを検索（再帰処理対応）"""
+        try:
+            folders = folder_name.split("/")
+            current_folder_name = folders[0]
+            remaining_folders = "/".join(folders[1:]) if len(folders) > 1 else None
 
-            if message.Attachments.Count > 0:
-                # 添付ファイルがある場合、フォルダを作成して保存
-                    #file_name = sanitize_filename(message.Subject)
-                attachment_folder_path = os.path.join(folder_path, "attachments")
-                save_attachments(attachment_folder_path, message)
-                
-            # 通常のテキストファイルに保存
-            file_name = os.path.join(folder_path, "本文.txt")
+            for folder in parent_folder.Folders:
+                if folder.Name == current_folder_name:
+                    print(f"フォルダ '{current_folder_name}' は既に存在します。")
+                    if remaining_folders:
+                        return self.find_folder(folder, remaining_folders)
+                    return folder
 
-            # メール内容を保存
-            save_email_content(file_name, message)
-    print("moved:", counter_move, "delete:", counter_delete, "remain:", counter_remain)
-    
-# アーカイブ処理を全アーカイブ対象フォルダに対して実行
-def do_all_folder(dic, folders):
-    for k in dic:
-        print(k)
-        if k != "del":
-            move_mail(dic, folders, target_folder=folders[k], view_none=False)
-    print("do all done.")
+            new_folder = parent_folder.Folders.Add(current_folder_name)
+            print(f"フォルダ '{current_folder_name}' を作成しました。")
+            if remaining_folders:
+                return self.find_folder(new_folder, remaining_folders)
+            return new_folder
 
-def check_dir():
-    """ローカルに保存するためのフォルダがあるか確認し、無い場合は新たに作成します。"""
-    for category, folder_name in folders.items():
-        # "del" カテゴリはスキップ
-        if category == "del":
-            continue
+        except Exception as e:
+            print(f"フォルダ作成中にエラーが発生しました: {e}")
+            return None
 
-        # 親フォルダかどうかの判定
-        if "#" in str(folder_name) or category == "archive":
-            path = os.path.join(dir_path, str(folder_name))
-        else:
-        #子フォルダの場合
-            path = os.path.join(path, str(folder_name))
+    def setup_local_paths(self):
+        """ローカルフォルダを作成"""
+        for category, rule in self.rules.items():
+            if category == "del":
+                continue
+            folder_structure = rule["folder"]
+            print(f"folder_structure is: {folder_structure}")
+            path = os.path.join(self.base_dir, *folder_structure.split("/"))
+            self.folders[category] = self.find_folder(self.root, folder_structure)
+            if category == "uncategorized" or category == "archive":
+                continue
+            if not os.path.isdir(path):
+                os.makedirs(path, exist_ok=True)
+                print(f"{folder_structure} フォルダを作成しました: {path}")
+            self.local_paths[category] = path
+        # for category, folder in self.folders.items():
+        #     print(f"カテゴリ: {category}, フォルダ名: {folder.Name}, フォルダパス: {folder.FolderPath}")
 
-        # フォルダが存在しない場合は作成
-        if not os.path.isdir(path):
-            os.makedirs(path)
-            print(f"{folder_name} フォルダを作成しました: {path}")
+    def categorize_email(self, message, dic):
+        """メールを仕分けルールに基づいて分類"""
+        for category, rule in dic.items():
+            if self.match_rule(message, rule):
+                return category
+        return None
+
+    @staticmethod
+    def match_rule(message, rule):
+        """メールがルールに一致するか判定"""
+        for subject in rule["subject"]:
+            if re.search(subject, message.Subject or ""):
+                return True
+        for address in rule["address"]:
+            if address in (message.SenderEmailAddress or ""):
+                return True
+        return False
+
+    def move_mail(self, dic, folders, target_folder, view_none=True, view_move=True, view_delete=True):
+        """メールを移動、削除、または未分類"""
+        counter_move = 0
+        counter_remain = 0
+        counter_delete = 0
+        list_move = []
+
+        sorted_messages = sorted(target_folder.Items, key=lambda msg: msg.ReceivedTime)
+        for message in sorted_messages:
+            key = self.categorize_email(message, dic)
+            if key == "del":
+                counter_delete += 1
+                list_move.append((message, None))
+            elif key is None or folders[key] is None or folders[key] == target_folder.FolderPath:
+                counter_remain += 1
+                list_move.append((message, folders["uncategorized"]))
+                if view_none:
+                    print(f"{counter_remain}, none, {folders["uncategorized"].Name} , {message.Subject}")
+            elif dic[key]["unread"] or not message.UnRead:
+                counter_move += 1
+                if view_move:
+                    print(folders[key].Name, message.Subject)
+                list_move.append((message, folders[key]))
+            # else:
+            #     counter_remain += 1
+            #     if view_none:
+            #         print("unread", message.Subject)
+
+        for message, dest in list_move:
+            if dest is None:
+                if view_delete:
+                    print("delete", message.Subject)
+                message.Delete()
+            else:
+                # print(dest, message.Subject)
+                message.UnRead = False
+                message.Move(dest)
+                if dest != folders["uncategorized"] and dest != folders["archive"]:
+                    folder_path = self.local_paths.get(key, None)
+                    if folder_path:
+                        self.saver.save_message(message, folder_path)
+
+        print("moved:", counter_move, "delete:", counter_delete, "archive:", counter_remain)
+
+def signal_handler(signum, frame):
+    """シグナルハンドラ"""
+    sys.exit(0)
+
+def initialize_signal_handler():
+    """シグナルハンドラを初期化"""
+    signal.signal(signal.SIGINT, signal_handler)
+
+def watch_json_folder():
+    print("jsonファイルの監視開始")
+    for changes in watch(R"C:\Users\pcrik\my_code\sort_emails_v2\src\json"): # 適宜書き換えてください
+        print("jsonファイルの変更を検出")
+        data_ready.set()  # JSONファイルの変更を検出
+
+class OutlookEventHandler:
+    """Outlookのイベントを処理するクラス"""
+    def __init__(self):
+        self.processor = None
+        self.inbox = None
+
+    def set_context(self, processor, inbox):
+        self.processor = processor
+        self.inbox = inbox
+
+    def OnNewMailEx(self, entry_id_collection):
+        """新しいメールを受信したときに呼び出されるイベント"""
+        print("新しいメールを受信しました。")
+        try:
+            # メールを仕分け
+            self.processor.move_mail(self.processor.rules, self.processor.folders, self.inbox)
+        except Exception as e:
+            print(f"メール処理中にエラーが発生しました: {e}")
+
+# スクリプトのエントリーポイント
+if __name__ == "__main__":
+    initialize_signal_handler()  # シグナルハンドラを初期化
+
+    try:
+        mail_app = win32com.client.DispatchWithEvents("Outlook.Application", OutlookEventHandler)
+        root = mail_app.Session.DefaultStore.GetRootFolder()
+        inbox = mail_app.GetNamespace("MAPI").GetDefaultFolder(6)
+
+        processor = MailProcessor(root=root)
+        if processor.load_rules():
+            print("仕分けルールの読み込みに失敗しました。")
+            sys.exit(1)
+
+        processor.setup_local_paths()
+        processor.move_mail(processor.rules, processor.folders, inbox)
+
+        json_watch_thread = threading.Thread(target=watch_json_folder, daemon=True) # JSONファイル監視スレッド    
+        json_watch_thread.start()  # JSONファイル監視スレッド開始
         
-        # 作成したフォルダのパスを local_path に保存
-        local_path[str(folder_name)] = path
+        handler = mail_app
+        handler.set_context(processor, inbox)  # イベントハンドラにコンテキストを設定
 
-def save_email_content(file_path, message):
-    """メール内容をファイルに書き込む（HTML形式省略版）"""
-    header_fields = [
-        ("送信日時", message.SentOn),
-        ("送信者", f"{message.Sender} <{message.SenderEmailAddress}>"),
-        ("宛先 (To)", message.To),
-        ("CC", message.CC),
-        ("BCC", message.BCC),
-        ("件名", message.Subject)
-    ]
-    with open(file_path, "w", encoding='utf-8') as f:
-        # 基本情報セクション
-        f.write("=== メール基本情報 ===\n")
-        for label, value in header_fields:
-            if value:  # 空の項目を非表示
-                f.write(f"・{label}: {value}\n")
-
-        # 本文セクション
-        f.write("\n=== 本文 ===\n")
-        f.write(f"{message.Body or '本文なし'}\n")
+         # Flask アプリケーションを別スレッドで実行
+        flask_thread = threading.Thread(target=lambda: app.run(debug=True, use_reloader=False), daemon=True)
+        flask_thread.start()
         
-    print(f"メールバックアップ完了: {file_path}")
+        print("メール監視を開始します...")
+        while True:
+            pythoncom.PumpWaitingMessages()  # COMメッセージを処理
+            
+            if data_ready.is_set():
+                data_ready.clear()  # イベントをリセット
+                if processor.load_rules():
+                    print("仕分けルールの読み込みに失敗しました。")
+                    sys.exit(1)
+                processor.setup_local_paths()
+                # print(processor.rules)
 
-
-def save_attachments(attachment_folder_path, message):
-    """添付ファイルを保存"""
-    os.makedirs(attachment_folder_path, exist_ok=True)
-    for attachment in message.Attachments:
-        attachment_file_path = os.path.join(attachment_folder_path, attachment.FileName)
-        attachment.SaveAsFile(attachment_file_path)
-        print(f"添付ファイルを保存しました: {attachment_file_path}")
-
-
-# ここからメイン関数となる部分
-# jsonからフォルダと
-dic, folders = load_json()
-# 日付が変わると日付を更新するようにする
-# ローカルフォルダのパスをmail.jsonにしたがって設定する
-check_dir()
-# print(local_path)
-# 受信フォルダに対して処理を行う場合
-move_mail(dic, folders)
-
-
-
-####### 作成に伴い不要となった関数群
-
-# def sanitize_filename(filename):
-#     # 使用できない文字のパターン
-#     invalid_chars = r'[\\/:*?"<>| ]'
-#     # 不正な文字を "_" に置き換える
-#     sanitized = re.sub(invalid_chars, '_', filename)
-#     # 先頭と末尾のスペースやドットを削除
-#     sanitized = sanitized.strip(' .')
-#     return sanitized
-
-# メールから日付を取得することにしたため削除
-# def set_date():
-#     d_today = datetime.date.today()
-#     # print(d_today+datetime.timedelta(days = 1))
-#     print(d_today)
-# schedule.every().day.at("00:00").do(set_date)
+            time.sleep(0.1)  # 0.1秒スリープしてCPU負荷を抑える
+    except KeyboardInterrupt:
+        print("CTRL+C による割り込みを検出しました。プログラムを終了します。")
+    finally:
+        print("リソースを解放しています...")
+        # 必要なリソース解放処理をここに記述
+        try:
+            del mail_app
+            pythoncom.CoUninitialize()
+        except Exception as e:
+            print(f"リソース解放中にエラーが発生しました: {e}")
+        finally:
+            print("リソース解放完了")
+        # プログラム終了メッセージ
+        print("プログラムを終了しました。")
